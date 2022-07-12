3f4a85a9717be7687e59cd52934c64a0d0e3368f
Derek Ma <danderekma@gmail.com>
2022-07-12 14:06:30 -0700

    Import fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -5,6 +7,15 @@ module.exports = {
       backgroundImage: {
         "grid-lg": "url('../assets/svgs/bg-grid-lg.svg')",
         "grid-sm": "url('../assets/svgs/bg-grid-sm.svg')"
+      },
+      fontFamily: {
+        "sf-bold": ["SF Pro Rounded Bold", ...defaultTheme.fontFamily.sans],
+        "sf-regular": [
+          "SF Pro Rounded Regular",
+          ...defaultTheme.fontFamily.sans
+        ],
+        "sf-light": ["SF Pro Rounded Light", ...defaultTheme.fontFamily.sans],
+        "sf-thin": ["SF Pro Rounded Thin", ...defaultTheme.fontFamily.sans]
       }
     }
   },
