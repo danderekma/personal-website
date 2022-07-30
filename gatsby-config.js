a3ed0d6b269cedf2c79603b3a63900a490d31270
Derek Ma <danderekma@gmail.com>
2022-07-30 14:47:10 -0700

    Remove font preloader

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-    "gatsby-plugin-preload-fonts",
     {
       resolve: "gatsby-plugin-use-dark-mode",
       options: {
