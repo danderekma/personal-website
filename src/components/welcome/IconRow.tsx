8ff4af226df3e32607bd768287a9c67512adaef6
Derek Ma <danderekma@gmail.com>
2023-03-17 08:48:51 -0700

    Migrate to Next

--- src/components/welcome/IconRow.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {
-    children?: React.ReactNode;
-};
-
-export default function IconRow({ children }: Props) {
-    return <div className="flex justify-center gap-3">{children}</div>;
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}"
+    ],
     darkMode: "class",
     theme: {
         extend: {
             backgroundImage: {
-                "grid-lg-light": "url('../assets/svgs/bg-grid-lg-light.svg')",
-                "grid-lg-dark": "url('../assets/svgs/bg-grid-lg-dark.svg')",
-                "grid-sm-light": "url('../assets/svgs/bg-grid-sm-light.svg')",
-                "grid-sm-dark": "url('../assets/svgs/bg-grid-sm-dark.svg')"
+                "grid-lg-light": "url('./public/svgs/bg-grid-lg-light.svg')",
+                "grid-lg-dark": "url('./public/svgs/bg-grid-lg-dark.svg')",
+                "grid-sm-light": "url('./public/svgs/bg-grid-sm-light.svg')",
+                "grid-sm-dark": "url('./public/svgs/bg-grid-sm-dark.svg')"
             },
             fontFamily: {
                 "sf-bold": [
