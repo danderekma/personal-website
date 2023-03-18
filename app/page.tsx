199f54d3616fac1b12565e6c0c9db2014fe05159
Derek Ma <danderekma@gmail.com>
2023-03-18 16:36:43 -0700

    Edit transition properties

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home(): React.ReactNode {
     }
 
     return (
-        <main className="bg-white bg-repeat transition-all-all grid-col bg-grid-sm-light dark:bg-grid-sm-dark sm:bg-grid-lg-light sm:dark:bg-grid-lg-dark dark:bg-gray">
+        <main className="transition-all bg-white bg-repeat grid-col bg-grid-sm-light dark:bg-grid-sm-dark sm:bg-grid-lg-light sm:dark:bg-grid-lg-dark dark:bg-gray">
             <div className="grid min-h-screen">
                 <div className="flex self-start justify-end w-full px-8 py-6 h-max">
                     <DarkModeToggle
@@ -50,24 +50,24 @@ export default function Home(): React.ReactNode {
                             className="w-56 rounded-full sm:w-80"
                         />
                     </div>
-                    <div className="relative flex flex-col transition-all justify-center gap-3 sm:gap-5 h-[250px] sm:h-[340px] bg-light-gray dark:bg-dark-gray bottom-7">
+                    <div className="relative flex flex-col justify-center h-64 gap-3 transition-all sm:gap-5 sm:h-80 bg-light-gray dark:bg-dark-gray bottom-7">
                         <div className="flex flex-col items-center sm:gap-2">
-                            <h1 className="text-3xl transition-all sm:text-5xl font-sf-bold dark:text-white">
-                                👋 Hi, I'm Derek!
+                            <h1 className="text-3xl transition-all duration-100 sm:text-5xl font-sf-bold dark:text-white">
+                                👋 Hi, I&apos;m Derek!
                             </h1>
-                            <h2 className="text-xl transition-all sm:text-3xl font-sf-regular dark:text-white">
-                                I'm a student developer at UC Davis.
+                            <h2 className="text-xl transition-all duration-100 sm:text-3xl font-sf-regular dark:text-white">
+                                I&apos;m a student developer at UC Davis.
                             </h2>
                         </div>
                         <div className="flex flex-col items-center">
                             <ul className="list-none">
-                                <li className="text-sm transition-all sm:text-xl font-sf-light dark:text-white">
+                                <li className="text-sm transition-all duration-100 sm:text-xl font-sf-light dark:text-white">
                                     ☕️ Fueled by coffee
                                 </li>
-                                <li className="text-sm transition-all sm:text-xl font-sf-light dark:text-white">
+                                <li className="text-sm transition-all duration-100 sm:text-xl font-sf-light dark:text-white">
                                     💼 Searching for internship...
                                 </li>
-                                <li className="text-sm transition-all sm:text-xl font-sf-light dark:text-white">
+                                <li className="text-sm transition-all duration-100 sm:text-xl font-sf-light dark:text-white">
                                     <a href="mailto:danderekma@gmail.com">
                                         ✉️ danderekma@gmail.com
                                     </a>
@@ -115,7 +115,7 @@ export default function Home(): React.ReactNode {
                 </div>
             </div>
             <div className="py-12 px-[15%] sm:py-24 sm:px-[20%]" id="projects">
-                <h1 className="py-6 text-2xl transition-all sm:text-4xl font-sf-bold dark:text-white">
+                <h1 className="py-6 text-2xl transition-all duration-100 sm:text-4xl font-sf-bold dark:text-white">
                     Projects
                 </h1>
                 <CardGrid>
@@ -151,7 +151,7 @@ export default function Home(): React.ReactNode {
                     />
                 </CardGrid>
             </div>
-            <footer className="w-full pb-4 text-sm text-center transition-all sm:text-base font-sf-thin text-black/25 dark:text-white/25">
+            <footer className="w-full pb-4 text-sm text-center transition-all duration-100 sm:text-base font-sf-thin text-black/25 dark:text-white/25">
                 Created with ❤️ by Derek Ma <br /> © 2022
             </footer>
         </main>
