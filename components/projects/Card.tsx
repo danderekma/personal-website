6401e894e2abd97b5099fd58ee769fc42e76ce64
Derek Ma <danderekma@gmail.com>
2023-03-21 14:31:07 -0700

    Customize card component

--- components/projects/Card.tsx
+++ components/projects/Card.tsx
@@ -7,11 +7,10 @@ type Props = {
 
 export default function Card({ name, description, image, link }: Props) {
     return (
-        <a
-            href={link}
-            className="flex flex-col rounded-3xl bg-light-gray dark:bg-dark-gray lg:flex-row lg:min-w-[32rem] lg:min-h-[18rem]"
-        >
-            <div className="lg:w-1/3">{image}</div>
+        <div className="flex flex-col rounded-3xl bg-light-gray dark:bg-dark-gray lg:flex-row lg:min-w-[32rem] lg:min-h-[18rem]">
+            <a href={link} className="lg:w-1/3">
+                {image}
+            </a>
             <div className="flex flex-col justify-center p-8 sm:p-12 lg:w-2/3">
                 <h1 className="pb-2 text-xl transition-all duration-100 sm:text-3xl font-sf-bold dark:text-white">
                     {name}
@@ -20,6 +19,6 @@ export default function Card({ name, description, image, link }: Props) {
                     {description}
                 </p>
             </div>
-        </a>
+        </div>
     );
 }
