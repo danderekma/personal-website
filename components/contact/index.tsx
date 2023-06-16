9ced18dead901bb3e240c8c68de13dcd2f60ec6a
Derek Ma <danderekma@gmail.com>
2023-06-16 00:06:50 -0700

    Reorganize codebase

---
+++ components/contact/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Contact() {
+    return (
+        <div className="py-12 px-[15%] sm:py-24 sm:px-[20%]" id="contact">
+            <h1 className="py-6 font-sf-bold text-2xl transition-all duration-100 dark:text-white sm:text-4xl">
+                Contact Me
+            </h1>
+        </div>
+    );
+}
