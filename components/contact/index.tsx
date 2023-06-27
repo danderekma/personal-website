99191978a5f1ebb6616d4a59e3c9e256b26ee091
Derek Ma <danderekma@gmail.com>
2023-06-27 14:45:33 +0800

    Use server component

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 export default function Contact() {
-    return (
-        <div className="py-12 px-[15%] sm:py-24 sm:px-[20%]" id="contact">
-            <h1 className="py-6 font-sf-bold text-2xl transition-all duration-100 dark:text-white sm:text-4xl">
-                Contact Me
-            </h1>
-        </div>
-    );
+  return (
+    <div className="py-12 px-[15%] sm:py-24 sm:px-[20%]" id="contact">
+      <h1 className="py-6 font-sf-bold text-2xl transition-all duration-100 dark:text-white sm:text-4xl">
+        Contact Me
+      </h1>
+      <label>First Name</label>
+      <input type="text" />
+    </div>
+  );
 }
