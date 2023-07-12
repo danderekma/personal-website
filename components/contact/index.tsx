a5e7d796f35342bd9a471793ef5ef6bb909bcc9d
Derek Ma <danderekma@gmail.com>
2023-07-11 23:13:07 -0700

    Spacing changes

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -2,12 +2,21 @@
 
 export default function Contact() {
   return (
-    <div className="py-12 px-[15%] sm:py-24 sm:px-[20%]" id="contact">
+    <div
+      className="mx-auto flex max-w-screen-2xl flex-col py-12 sm:py-24"
+      id="contact"
+    >
       <h1 className="py-6 font-sf-bold text-2xl transition-all duration-100 dark:text-white sm:text-4xl">
         Contact Me
       </h1>
-      <label>First Name</label>
-      <input type="text" />
+      <label className="font-sf-regular dark:text-neutral-400">
+        First Name
+      </label>
+      <input
+        className="h-9 w-2/5 rounded-lg border border-neutral-300 bg-white px-2 font-sf-light"
+        type="text"
+        placeholder="Enter here"
+      />
     </div>
   );
 }
