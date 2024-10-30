fc15d58ff8b858e70560f6f5370f0b7a9493938c
Derek Ma <danderekma@gmail.com>
2024-10-30 12:16:43 -0700

    Change output type

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "server",
+  output: "static",
   adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime]
