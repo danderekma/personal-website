47c4b5b6e5edc7e9d6e1a62cd74efd1efeb2962b
Derek Ma <danderekma@gmail.com>
2024-10-30 12:20:53 -0700

    Revert output type

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "static",
+  output: "hybrid",
   adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime]
